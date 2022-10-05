2d481ddb02541bf53939e6278daa02a9c1460ba4
P Padmaprasad Shenoy <92221430+ppadmaprasadshenoy@users.noreply.github.com>
2022-10-05 09:57:50 +0530

    Add files via upload

---
+++ const.js
@@ -0,0 +1,40 @@
+// console.log(console)
+// console.log(console.log)
+// console.log(typeof null)
+// console.log(typeof 123)
+// console.log(typeof abc)
+// console.log(typeof true)
+// console.log(typeof undefined)
+// console.log(function(){})
+// console.log(typeof {})
+// console.log(typeof [])
+
+// //declaration
+// const a;
+// console.log(a);
+// //initialization
+// const b;
+// b = "hello world";
+// console.log(b);
+
+// //both declaration and initialization
+// const c =20;
+// console.log(c);
+
+// //redeclaration
+// const d = "canara";
+// const d = "CANARA";
+// console.log(d);
+
+// //reinitialization
+// const e;
+// e = "morning";
+// console.log(e);
+// e = "night";
+// console.log(e);
+
+// //hoisting
+// console.log(f);
+// const f = "namaste";
+
+
---
+++ index.js
@@ -0,0 +1,38 @@
+// console.log(console)
+// console.log(console.log)
+// console.log(typeof null)
+// console.log(typeof 123)
+// console.log(typeof abc)
+// console.log(typeof true)
+// console.log(typeof undefined)
+// console.log(function(){})
+// console.log(typeof {})
+// console.log(typeof [])
+// //declaration
+// var a;
+// console.log(a);
+// //initialization
+// var b;
+// b = "hello world";
+// console.log(b);
+
+// //both declaration and initialization
+// var c =20;
+// console.log(c);
+
+// //redeclaration
+// var d = "canara";
+// var d = "CANARA";
+// console.log(d);
+
+// //reinitialization
+// var e;
+// e = "morning";
+// console.log(e);
+// e = "night";
+// console.log(e);
+
+// //hoisting
+// console.log(f);
+// var f = "namaste";
+
